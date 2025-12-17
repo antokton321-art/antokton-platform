050f9cf19d55beaec062a8b93b105df17aca1d7a
antokton321-art <antokton321@gmail.com>
2025-12-17 05:36:58 +0100

    Fix type definition for PostPage params

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,7 +2,7 @@ import CommentList from '@/components/CommentList'
 import PostCard from '@/components/PostCard'
 import { getPostById } from '@/lib/firestore'
 
-export default async function PostPage({ params }: { params:Promise< { id: string> } }){
+export default async function PostPage({ params }: { params:Promise< { id: string}>  }){
   const post = await getPostById((await params).id)  if(!post) return <div className="card">Post not found</div>
 
   return (
